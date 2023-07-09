b51e9b9906ea4ed12698066e7c849c000be60a9a
Hossein <h.dvd13@gmail.com>
2023-07-09 14:55:49 -0700

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib = [0, 1]
+        i = 2
+        while i < length:
+            fib.append(fib[i-1] + fib[i-2])
+            i += 1
+        
+        print(fib)
